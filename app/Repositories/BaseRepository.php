7185ed3b4fab599dcb91f281d10150b5f1393798
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-12-20 20:33:25 +0300

    feat(repositories): create base repository interface and abstract class
    - Create RepositoryInterface with common CRUD methods
    - Create BaseRepository abstract class implementing interface
    - Establish foundation for domain-specific repositories

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Repositories\Contracts\RepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseRepository implements RepositoryInterface
+{
+    public function __construct(
+        protected Model $model
+    ) {}
+
+    public function all(): Collection
+    {
+        return $this->model->all();
+    }
+
+    public function find(int $id): ?Model
+    {
+        return $this->model->find($id);
+    }
+
+    public function create(array $data): Model
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(Model $model, array $data): Model
+    {
+        $model->update($data);
+
+        return $model;
+    }
+
+    public function delete(Model $model): bool
+    {
+        return $model->delete();
+    }
+
+    public function count(): int
+    {
+        return $this->model->count();
+    }
+}
+
